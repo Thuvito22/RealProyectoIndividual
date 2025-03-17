3ad256b6bad465bd0ec069c63e736d71ccd6b7e7
DESKTOP-M325FMV\Fer_Y <notengo@gmail.com>
2025-03-17 15:49:14 +0100

    organizados los ficheros json, router.ts y formulario componente ts todo ok

--- miappFRONT/src/app/app.routes.ts
+++ miappFRONT/src/app/app.routes.ts
@@ -1,13 +1,29 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 import { FormularioordenadorComponent } from './formularioordenador/formularioordenador.component';
 import { ListaordenadorComponent } from './listaordenador/listaordenador.component';
 
-export const routes: Routes = [
-
-    { path: "listaordenador", component: ListaordenadorComponent },
-    { path: "formularioordenador", component: FormularioordenadorComponent },
-    { path: "", component: ListaordenadorComponent }
-
-
-
+const routes: Routes = [
+  { path: 'listaordenador', component: ListaordenadorComponent },
+  { path: 'formularioordenador', component: FormularioordenadorComponent },
+  { path: '', redirectTo: 'listaordenador', pathMatch: 'full' }
 ];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FormularioordenadorComponent,
+    ListaordenadorComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(routes)  
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
--- miappFRONT/src/app/formularioordenador/formularioordenador.component.ts
+++ miappFRONT/src/app/formularioordenador/formularioordenador.component.ts
@@ -3,36 +3,59 @@ import { FormsModule } from '@angular/forms';
 import { Ordenador } from '../ordenador';
 import { OrdenadorRestService } from '../ordenador-rest.service';
 import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-formularioordenador',
+  standalone: true,
   imports: [FormsModule],
   templateUrl: './formularioordenador.component.html',
-  styleUrl: './formularioordenador.component.scss'
+  styleUrls: ['./formularioordenador.component.scss']
 })
 export class FormularioordenadorComponent {
 
   ordenador: Ordenador = {} as Ordenador; 
 
-  constructor(private ordenadorRestService: OrdenadorRestService, private router: Router) {}
+  constructor(private readonly ordenadorRestService: OrdenadorRestService, private readonly router: Router) {}
 
   public insertar(): void {
-    this.ordenadorRestService.insertar(this.ordenador).subscribe(
-      () => {
-        this.mostrarMensaje('Ordenador guardado con éxito');
-        this.router.navigate(['/listaordenador']);
+    if (this.ordenador.numserie < 0) {
+      Swal.fire("El campo 'Número de Serie' debe ser mayor o igual a cero.");
+      return;
+    }
+    if (this.ordenador.precio < 0) {
+      Swal.fire("El campo 'Precio' debe ser mayor o igual a cero");
+      return;
+    }
+
+    this.ordenadorRestService.insertar(this.ordenador).subscribe({
+      next: (datos) => {
+        Swal.fire({
+          title: 'Éxito',
+          text: 'Ordenador guardado con éxito',
+          icon: 'success',
+          confirmButtonText: 'OK'
+        }).then(() => {
+          this.router.navigate(['/listaordenador']);  // con this router navigate lo mandamos otra vez al array
+        });
       },
-      (error) => {
-        if (error.status === 500) { 
-          this.mostrarMensaje('Error: Número de serie duplicado. Ingrese un número de serie único.');
+      error: (error) => {
+        if (error.status === 500) {
+          Swal.fire({
+            title: 'Error',
+            text: 'Número de serie duplicado. Por favor, ingrese un número de serie único.',
+            icon: 'error',
+            confirmButtonText: 'OK'
+          });
         } else {
-          this.mostrarMensaje('Error: Ocurrió un problema al guardar el ordenador. Intente nuevamente.');
+          Swal.fire({
+            title: 'Error',
+            text: 'Ocurrió un error al guardar el ordenador. Por favor, pruebe nuevamente.',
+            icon: 'error',
+            confirmButtonText: 'OK'
+          });
         }
       }
-    );
-  }
-
-  private mostrarMensaje(mensaje: string): void {
-    alert(mensaje);
+    });
   }
 }
