657f53626fe2129695c3de27f89192169cfe1c38
DESKTOP-M325FMV\Fer_Y <notengo@gmail.com>
2025-03-17 18:43:35 +0100

    Cambio en logica de insertar  minimo numero de serie 0 o mas

--- miappFRONT/src/app/formularioordenador/formularioordenador.component.ts
+++ miappFRONT/src/app/formularioordenador/formularioordenador.component.ts
@@ -17,7 +17,7 @@ export class FormularioordenadorComponent {
   ordenador: Ordenador = {} as Ordenador;
   marcas = ['HP', 'Dell', 'Lenovo', 'Asus', 'Acer'];
   modelos = ['Modelo A', 'Modelo B', 'Modelo C', 'Modelo D', 'Modelo E'];
-  
+
   // Agrega esta propiedad para almacenar el mensaje de éxito
   successMessage: string | null = null; // Definir successMessage
 
@@ -27,28 +27,35 @@ export class FormularioordenadorComponent {
   ) {}
 
   public insertar(): void {
-    if (this.ordenador.numserie < 0) {
-      Swal.fire("El campo 'Número de Serie' debe ser mayor o igual a cero.");
+    // Verificar si el número de serie es válido
+    if (!this.ordenador.numserie || this.ordenador.numserie < 0) {
+      Swal.fire("El campo 'Número de Serie' es obligatorio y debe ser mayor o igual a cero.");
       return;
     }
+
+    // Verificar si el precio es válido
     if (this.ordenador.precio < 0) {
       Swal.fire("El campo 'Precio' debe ser mayor o igual a cero");
       return;
     }
 
+    // Llamada al servicio para guardar el ordenador
     this.ordenadorRestService.insertar(this.ordenador).subscribe({
       next: (datos) => {
-        this.successMessage = 'Ordenador guardado con éxito'; // Asigna el mensaje de éxito
+        // Mostrar mensaje de éxito
+        this.successMessage = 'Ordenador guardado con éxito';
         Swal.fire({
           title: 'Éxito',
           text: this.successMessage,
           icon: 'success',
           confirmButtonText: 'OK'
         }).then(() => {
+          // Redirigir a la lista de ordenadores
           this.router.navigate(['/listaordenador']);
         });
       },
       error: (error) => {
+        // Si hay un error de servidor (500), mostrar mensaje de duplicado
         if (error.status === 500) {
           Swal.fire({
             title: 'Error',
@@ -57,6 +64,7 @@ export class FormularioordenadorComponent {
             confirmButtonText: 'OK'
           });
         } else {
+          // Si es otro error, mostrar mensaje genérico
           Swal.fire({
             title: 'Error',
             text: 'Ocurrió un error al guardar el ordenador. Por favor, pruebe nuevamente.',
